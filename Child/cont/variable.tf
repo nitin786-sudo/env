f50d73de943eb7112e1458b65fbc1f42b86fb8b7
nitin786-sudo <nitin.nitesh24@gmail.com>
2025-10-23 16:55:08 +0530

    frsehadded

---
+++ Child/cont/variable.tf
@@ -0,0 +1,10 @@
+variable "rg_name" {
+  type        = string
+  description = "The name of the resource group"
+  
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "The name of the storage account"
+}
---
+++ Child/stg/variable.tf
@@ -0,0 +1,15 @@
+variable "rg_name" {
+  type        = string
+  description = "The name of the resource group"
+  
+}
+
+variable "rg_location" {
+  type        = string
+  description = "The location of the resource group"
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "The name of the storage account"
+}
